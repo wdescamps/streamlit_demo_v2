3c5dd47380ed90cf1a2065305b490f01ac993c3f
Your Name <william.descamps@gmail.com>
2025-03-07 10:01:58 +0100

    changed page structure

---
+++ app/page1.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+def get_map_data():
+
+    return pd.DataFrame(
+            np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+            columns=['lat', 'lon']
+        )
+
+df = get_map_data()
+
+st.map(df)
